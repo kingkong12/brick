dc060eaedce1c3a88dbde83198234fd39916ebbc
msvasani777 <msvasani777@gmail.com>
2024-01-02 02:02:48 -0500

    removed default main screen

--- next.config.js
+++ next.config.js
@@ -1,14 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // async redirects() {
-  //     return [
-  //       {
-  //         source: '/',
-  //         destination: '/auth/login',
-  //         permanent: false, // Keep this as false for a non-permanent redirect
-  //       },
-  //     ];
-  //   },
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/auth/login',
+        permanent: false, // Keep this as false for a non-permanent redirect
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
